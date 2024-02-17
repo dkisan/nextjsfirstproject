6482206d97e9cb99d909b308d7ef82982b911df5
DebasishKisan1 <debasish.kisan@gmail.com>
2024-02-17 06:35:08 +0530

    linking page

--- pages/aboutus/developer/index.js
+++ pages/aboutus/developer/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 const Developerpage = () => {
-    return <h1>Debasish Kisan</h1>
+    const devList = [
+        { id: 1, name: 'Debasish Kisan' },
+        { id: 2, name: 'Dk' },
+        { id: 3, name: 'Debasish' }
+    ]
+
+
+    return (
+        <div>
+            {
+                devList.map(d => {
+                    return <h2 key={d.id}><Link href={`/aboutus/developer/${d.id}`}>{d.name}</Link></h2>
+                })
+            }
+        </div>
+    )
 }
 
 export default Developerpage;
